384d4dc9b85233e514e019e9d2056e4cff272312
fizik003 <kassik0boks@gmail.com>
2020-04-15 02:14:50 +0300

    feat: install webpack, eslint, babel

---
+++ src/script.js
@@ -0,0 +1,3 @@
+import './styles/style.css';
+
+const a = document.getElementsByTagName('h1');
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/script.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   options: {
+      //     // eslint options (if necessary)
+      //   },
+      // },
+      {
+        test: /\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|wolf|wolf2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
