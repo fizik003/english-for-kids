abb60a01479c960ad1f7817b06bde6884472abf8
fizik003 <kassik0boks@gmail.com>
2020-04-17 03:09:04 +0300

    feat: add burger menu

--- dist/main.53c22b015f2bd0c8f8cc.js
+++ dist/main.53c22b015f2bd0c8f8cc.js
@@ -97,6 +97,17 @@ eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../c
 
 /***/ }),
 
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/styles/hamburger.css":
+/*!************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/styles/hamburger.css ***!
+  \************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\n// Module\nexports.push([module.i, \".hamburger{\\r\\n    position: absolute;\\r\\n    width: 25px;\\r\\n    height: 25px;\\r\\n    left: 30px;\\r\\n    display: inline-block;\\r\\n    margin-top: 5px;\\r\\n    z-index: 3;\\r\\n}\\r\\n\\r\\n.hamburger::before {\\r\\n    content: '';\\r\\n    position: absolute;\\r\\n    display: inline-block;\\r\\n    height: 4px;\\r\\n    width: 100%;\\r\\n    border-radius: 5px;\\r\\n    background-color: rgb(255, 255, 255);\\r\\n    top: calc(50% - 3.1px);\\r\\n}\\r\\n\\r\\n.hamburger::after{\\r\\n    content: '';\\r\\n    position: absolute;\\r\\n    display: inline-block;\\r\\n    height: 4px;\\r\\n    width: 100%;\\r\\n    border-radius: 5px;\\r\\n    background-color: rgb(255, 255, 255);\\r\\n    bottom: 2px;\\r\\n\\r\\n}\\r\\n\\r\\n.hamburger_line {\\r\\n    top: 0;\\r\\n    position: absolute;\\r\\n    display: inline-block;\\r\\n    height: 4px;\\r\\n    width: 100%;\\r\\n    border-radius: 5px;\\r\\n    background-color: rgb(255, 255, 255);\\r\\n}\\r\\n\\r\\n.hamburger-transform {\\r\\n    margin-top: -12px;\\r\\n    transform: rotate(90deg);\\r\\n}\\r\\n\\r\\n.burger{\\r\\n    position: relative;\\r\\n}\", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/styles/hamburger.css?./node_modules/css-loader/dist/cjs.js");
+
+/***/ }),
+
 /***/ "./node_modules/css-loader/dist/cjs.js!./src/styles/style.css":
 /*!********************************************************************!*\
   !*** ./node_modules/css-loader/dist/cjs.js!./src/styles/style.css ***!
@@ -104,7 +115,18 @@ eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../c
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nvar ___CSS_LOADER_AT_RULE_IMPORT_0___ = __webpack_require__(/*! -!../../node_modules/css-loader/dist/cjs.js!../../node_modules/normalize.css */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/normalize.css/normalize.css\");\nvar ___CSS_LOADER_GET_URL_IMPORT___ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/getUrl.js */ \"./node_modules/css-loader/dist/runtime/getUrl.js\");\nvar ___CSS_LOADER_URL_IMPORT_0___ = __webpack_require__(/*! ../assets/123.png */ \"./src/assets/123.png\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\nexports.i(___CSS_LOADER_AT_RULE_IMPORT_0___);\nvar ___CSS_LOADER_URL_REPLACEMENT_0___ = ___CSS_LOADER_GET_URL_IMPORT___(___CSS_LOADER_URL_IMPORT_0___);\n// Module\nexports.push([module.i, \"h1{\\r\\n    background-color: red;\\r\\n\\r\\n}\\r\\n\\r\\n.logo{\\r\\n    background-image: url(\" + ___CSS_LOADER_URL_REPLACEMENT_0___ + \");\\r\\n    width: 400px;\\r\\n    height: 500px;\\r\\n    background-repeat: no-repeat;\\r\\n}\", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/styles/style.css?./node_modules/css-loader/dist/cjs.js");
+eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nvar ___CSS_LOADER_AT_RULE_IMPORT_0___ = __webpack_require__(/*! -!../../node_modules/css-loader/dist/cjs.js!../../node_modules/normalize.css */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/normalize.css/normalize.css\");\nvar ___CSS_LOADER_AT_RULE_IMPORT_1___ = __webpack_require__(/*! -!../../node_modules/css-loader/dist/cjs.js!./switcher.css */ \"./node_modules/css-loader/dist/cjs.js!./src/styles/switcher.css\");\nvar ___CSS_LOADER_AT_RULE_IMPORT_2___ = __webpack_require__(/*! -!../../node_modules/css-loader/dist/cjs.js!./hamburger.css */ \"./node_modules/css-loader/dist/cjs.js!./src/styles/hamburger.css\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\nexports.i(___CSS_LOADER_AT_RULE_IMPORT_0___);\nexports.i(___CSS_LOADER_AT_RULE_IMPORT_1___);\nexports.i(___CSS_LOADER_AT_RULE_IMPORT_2___);\n// Module\nexports.push([module.i, \".header-container {\\r\\n    background-color: aqua;\\r\\n    /* height: 300px; */\\r\\n    width: 100%;\\r\\n    display: flex;\\r\\n    justify-content: space-between;\\r\\n    max-width: 1350px;\\r\\n    margin-left: auto;\\r\\n    margin-right: auto;\\r\\n    /* position: relative; */\\r\\n}\\r\\n\\r\\n.navigation{\\r\\n    position: absolute;\\r\\n    height: 100%;\\r\\n    width: 300px;\\r\\n    background-color: lightcoral;\\r\\n    left: -300px;\\r\\n    transition: .5s all;\\r\\n    \\r\\n    \\r\\n}\\r\\n\\r\\n\\r\\n\\r\\n.show-burger{\\r\\n    left: 0px;\\r\\n    animation-duration: 0.5s;\\r\\n    animation-name: slidein;\\r\\n}\\r\\n\\r\\n.nav{\\r\\n    list-style-type: none;\\r\\n    display: flex;\\r\\n    flex-direction: column;\\r\\n    padding: 125px 50px 50px;\\r\\n}\\r\\n\\r\\n.nav-item{\\r\\n    padding-top: 10px;\\r\\n    font-size: 22px;\\r\\n}\\r\\n\\r\\n\\r\\n@keyframes slidein {\\r\\n    from {\\r\\n        left: -300px;\\r\\n    }\\r\\n    to {\\r\\n        left: 0;\\r\\n    }\\r\\n}\\r\\n\\r\\n\", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/styles/style.css?./node_modules/css-loader/dist/cjs.js");
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/styles/switcher.css":
+/*!***********************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/styles/switcher.css ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\n// Module\nexports.push([module.i, \".container {\\r\\n    padding: 25px;\\r\\n  }\\r\\n  \\r\\n /* Modified CSS for on/off switch */\\r\\n  .onoffswitch {\\r\\n      position: relative; \\r\\n      width: 78px;\\r\\n      -webkit-user-select:none; -moz-user-select:none; -ms-user-select: none;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-checkbox {\\r\\n      display: none;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-label {\\r\\n      display: block; \\r\\n      overflow: hidden; \\r\\n      cursor: pointer;\\r\\n      border: 1px solid #999999; \\r\\n      border-radius: 20px;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-inner {\\r\\n      width: 200%; margin-left: -100%;\\r\\n      -moz-transition: margin 0.3s ease-in 0s; -webkit-transition: margin 0.3s ease-in 0s;\\r\\n      -o-transition: margin 0.3s ease-in 0s; transition: margin 0.3s ease-in 0s;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-inner:before, .onoffswitch-inner:after {\\r\\n      float: left; width: 50%; height: 30px; padding: 0; line-height: 30px;\\r\\n      font-size: 15px; color: white; font-family: Trebuchet, Arial, sans-serif; font-weight: bold;\\r\\n      -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-inner:before {\\r\\n      content: \\\"Play\\\";\\r\\n      padding-left: 11px;\\r\\n      background-color: #00A600; color: #FFFFFF;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-inner:after {\\r\\n      content: \\\"Train\\\";\\r\\n      padding-right: 11px;\\r\\n      background-color: #EEEEEE; color: #999999;\\r\\n      text-align: right;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-switch {\\r\\n      width: 26px; margin: 2px;\\r\\n      background: #FFFFFF;\\r\\n      border: 2px solid #999999; border-radius: 20px;\\r\\n      position: absolute; top: 0; bottom: 0; right: 44px;\\r\\n      -moz-transition: all 0.3s ease-in 0s; -webkit-transition: all 0.3s ease-in 0s;\\r\\n      -o-transition: all 0.3s ease-in 0s; transition: all 0.3s ease-in 0s; \\r\\n  }\\r\\n  \\r\\n  .onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {\\r\\n      margin-left: 0;\\r\\n  }\\r\\n  \\r\\n  .onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {\\r\\n      right: 0px; \\r\\n  }\\r\\n  \", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/styles/switcher.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -120,18 +142,6 @@ eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n
 
 /***/ }),
 
-/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
-/*!********************************************************!*\
-  !*** ./node_modules/css-loader/dist/runtime/getUrl.js ***!
-  \********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-eval("\n\nmodule.exports = function (url, options) {\n  if (!options) {\n    // eslint-disable-next-line no-param-reassign\n    options = {};\n  } // eslint-disable-next-line no-underscore-dangle, no-param-reassign\n\n\n  url = url && url.__esModule ? url.default : url;\n\n  if (typeof url !== 'string') {\n    return url;\n  } // If url is already wrapped in quotes, remove them\n\n\n  if (/^['\"].*['\"]$/.test(url)) {\n    // eslint-disable-next-line no-param-reassign\n    url = url.slice(1, -1);\n  }\n\n  if (options.hash) {\n    // eslint-disable-next-line no-param-reassign\n    url += options.hash;\n  } // Should url be wrapped?\n  // See https://drafts.csswg.org/css-values-3/#urls\n\n\n  if (/[\"'() \\t\\n]/.test(url) || options.needQuotes) {\n    return \"\\\"\".concat(url.replace(/\"/g, '\\\\\"').replace(/\\n/g, '\\\\n'), \"\\\"\");\n  }\n\n  return url;\n};\n\n//# sourceURL=webpack:///./node_modules/css-loader/dist/runtime/getUrl.js?");
-
-/***/ }),
-
 /***/ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js":
 /*!****************************************************************************!*\
   !*** ./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js ***!
@@ -144,18 +154,6 @@ eval("\n\nvar isOldIE = function isOldIE() {\n  var memo;\n  return function mem
 
 /***/ }),
 
-/***/ "./src/assets/123.png":
-/*!****************************!*\
-  !*** ./src/assets/123.png ***!
-  \****************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"3e62593f3ea6a1851b8ade65a1c7f8aa.png\");\n\n//# sourceURL=webpack:///./src/assets/123.png?");
-
-/***/ }),
-
 /***/ "./src/script.js":
 /*!***********************!*\
   !*** ./src/script.js ***!
@@ -164,7 +162,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ 
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/style.css */ \"./src/styles/style.css\");\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_style_css__WEBPACK_IMPORTED_MODULE_0__);\n\nvar a = document.getElementsByTagName('h1');\n\n//# sourceURL=webpack:///./src/script.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/style.css */ \"./src/styles/style.css\");\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_style_css__WEBPACK_IMPORTED_MODULE_0__);\n\nvar burger_line = document.querySelector('.hamburger');\nvar burger_all = document.querySelector('.navigation');\nburger_line.addEventListener('click', closeBurg);\n\nfunction closeBurg() {\n  if (burger_all.classList.contains('show-burger')) {\n    burger_all.classList.remove('show-burger');\n  } else {\n    burger_all.classList.add('show-burger');\n  }\n}\n\nconsole.log('ffffffff');\n\n//# sourceURL=webpack:///./src/script.js?");
 
 /***/ }),
 
--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
 import './styles/style.css';
 
-const a = document.getElementsByTagName('h1');
+const burger_line = document.querySelector('.hamburger');
+const burger_all = document.querySelector('.navigation')
+
+burger_line.addEventListener('click', closeBurg)
+
+function closeBurg() {
+  if (burger_all.classList.contains('show-burger')){
+    burger_all.classList.remove('show-burger');
+    
+  }
+  else{
+    
+    burger_all.classList.add('show-burger');
+  }
+}
+
+
+console.log('ffffffff');
+
+
